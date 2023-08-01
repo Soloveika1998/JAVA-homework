a33978f191cbffc34669395b8b8c81b3270e931f
AlexSol <solovejaleksej5@gmail.com>
2023-08-01 17:08:25 +0300

    add App.java

---
+++ src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.addSorted(1);
+        list.addSorted(2);
+        list.addSorted(3);
+        list.addSorted(4);
+        list.addSorted(5);
+        list.addSorted(6);
+        list.addSorted(7);
+        list.addSorted(8);
+        list.addSorted(9);
+        list.addSorted(10);
+        list.addSorted(11);
+        list.addSorted(12);
+        list.addSorted(13);
+        
+        list.print();
+        list.revers();
+        list.print();
+    }
+}
